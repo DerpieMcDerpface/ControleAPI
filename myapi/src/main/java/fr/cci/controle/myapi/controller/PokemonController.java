f44177316719c5f3ca56f0df6b161c41f847741f
Ben <benjamin.mouginot@live.fr>
2019-07-11 16:07:38 +0200

    Adding @GetMapping in PokemonController
    * Added a @GetMapping for pokedex/pokemon/type/{id}
    * Slight changes to PokemonPage.html

--- myapi/src/main/java/fr/cci/controle/myapi/controller/PokemonController.java
+++ myapi/src/main/java/fr/cci/controle/myapi/controller/PokemonController.java
@@ -1,12 +1,18 @@
 package fr.cci.controle.myapi.controller;
 
+import fr.cci.controle.myapi.model.Pokemon;
+import fr.cci.controle.myapi.model.Type;
 import fr.cci.controle.myapi.repository.PokemonRepository;
 import fr.cci.controle.myapi.repository.TypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "/pokedex/pokemon")
@@ -18,9 +24,12 @@ public class PokemonController {
     private TypeRepository typeRepository;
 
 
-    @GetMapping(path ="/type/{id}")
-    public String showPokemonByTypeId(@RequestParam int typeId){
-
+    @GetMapping(path ="/type/{typeId}")
+    public String showPokemonByTypeId(@PathVariable int typeId, Model model){
+        Type type = typeRepository.findById(typeId);
+        List<Pokemon> pokemonList = pokemonRepository.findByType(type);
+        model.addAttribute("type", type);
+        model.addAttribute("pokemonList", pokemonList);
         return "PokemonPage";
     }
 
--- myapi/src/main/java/fr/cci/controle/myapi/repository/PokemonRepository.java
+++ myapi/src/main/java/fr/cci/controle/myapi/repository/PokemonRepository.java
@@ -2,11 +2,17 @@ package fr.cci.controle.myapi.repository;
 
 
 import fr.cci.controle.myapi.model.Pokemon;
+import fr.cci.controle.myapi.model.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PokemonRepository extends JpaRepository<Pokemon, Integer> {
 
     Pokemon findById(int id);
+
+    List<Pokemon> findByType(Type type);
+
 }
