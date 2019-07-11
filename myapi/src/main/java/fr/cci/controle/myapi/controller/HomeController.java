e489d9d5ccd36e28c90a792a884da42ce57d5944
Ben <benjamin.mouginot@live.fr>
2019-07-11 14:16:42 +0200

    Creation of the 3 controllers
    * Added 3 classes to /controller
    * These will be the controllers for our application:
    - HomeController handling the homepage
    - PokemonController for the page showing the different Pokemons
    - TypeController for the page showing the types

---
+++ myapi/src/main/java/fr/cci/controle/myapi/controller/HomeController.java
@@ -0,0 +1,24 @@
+package fr.cci.controle.myapi.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(path = "/pokedex")
+public class HomeController {
+
+    @RequestMapping("/accueil")
+    public String showHomepage(@RequestParam(required = false) String page, Model model){
+        if (page != null || page == "2#") {
+            model.addAttribute("page", page);
+            return "Homepage";
+        } else {
+            page = "1";
+            model.addAttribute("page", page);
+            return "Homepage";
+        }
+    }
+}
---
+++ myapi/src/main/java/fr/cci/controle/myapi/controller/PokemonController.java
@@ -0,0 +1,14 @@
+package fr.cci.controle.myapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(path = "/pokemon")
+public class PokemonController {
+
+
+
+
+}
