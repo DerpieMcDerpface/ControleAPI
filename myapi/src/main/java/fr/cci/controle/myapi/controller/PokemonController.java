bfe8ac97a64cd8e7abfa51977fa21722124d0833
Ben <benjamin.mouginot@live.fr>
2019-07-11 14:22:01 +0200

    Dev. Controller
    * Added repositories to the Controller where it was needed
    * Added the first GetMapping to TypeController

--- myapi/src/main/java/fr/cci/controle/myapi/controller/PokemonController.java
+++ myapi/src/main/java/fr/cci/controle/myapi/controller/PokemonController.java
@@ -1,5 +1,8 @@
 package fr.cci.controle.myapi.controller;
 
+import fr.cci.controle.myapi.repository.PokemonRepository;
+import fr.cci.controle.myapi.repository.TypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping(path = "/pokemon")
 public class PokemonController {
+
+    @Autowired
+    private PokemonRepository pokemonRepository;
+    @Autowired
+    private TypeRepository typeRepository;
+
+
 
 
 
--- myapi/src/main/java/fr/cci/controle/myapi/controller/TypeController.java
+++ myapi/src/main/java/fr/cci/controle/myapi/controller/TypeController.java
@@ -1,12 +1,29 @@
 package fr.cci.controle.myapi.controller;
 
 
+import fr.cci.controle.myapi.repository.PokemonRepository;
+import fr.cci.controle.myapi.repository.TypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "/type")
 public class TypeController {
+
+    @Autowired
+    private PokemonRepository pokemonRepository;
+    @Autowired
+    private TypeRepository typeRepository;
+
+
+    @GetMapping(path ="/all")
+    public String showAllType(Model model){
+        model.addAttribute("listType", typeRepository.findAll());
+        return "TypePage";
+    }
 
 
 
